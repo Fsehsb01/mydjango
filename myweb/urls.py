607e6a4b519b57d0b580171eae0ec7d88d7cbafc
27142 <2714214046@qq.com>
2022-06-23 01:29:05 +0800

    测试0.0.2

--- myweb/urls.py
+++ myweb/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name='myweb'
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('', views.look,name='look'),
-    path('detail/', views.detail, name='detail'),
+    path('look/', views.look,name='look'),
+    path('', views.detail, name='detail'),
+    path('hello/',views.lokk,name='lokk'),
 ]
--- myweb/views.py
+++ myweb/views.py
@@ -1,26 +1,28 @@
-from django.shortcuts import render, reverse, get_object_or_404
+from django.shortcuts import render, reverse, get_object_or_404, redirect
 from django.http import HttpRequest, HttpResponse
 from .models import lo
 
 
 # Create your views here.
+def lokk(request):
+    return render(request, 'holle.html')
 def look(request):
     name = request.POST.get('name')
     namet = request.POST.get('namet')
-    if name==''and namet=='' :
-        return HttpResponse('至少要一个有东西吧')
+    if name==None and namet==None:
+        return HttpResponse('1')
     else:
-        lo.objects.filter(name=name, namet=namet).first()
-        lo.objects.create(name=name, namet=namet)
-        return render(request, 'holle.html')
+        lo.objects.create(name=name,namet=namet)
+
+    return redirect(reverse('myweb:detail'))
 
 
 def index(request):
-    q = lo.objects.filter(stat='1')
-
+    q = lo.objects.filter(stat__contains='1')
+    print(lo.objects.filter(stat__contains='1'))
     return render(request, 'index.html', {'q': q})
 
 
 def detail(request):
-    q=lo.objects.all()
+    q = lo.objects.filter(stat__contains='1')
     return render(request, 'myweb/detail.html', {'q': q})
